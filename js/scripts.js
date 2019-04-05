ad22dc8c96449de0a8cf784a416840328e894058
Hannah Melendy <hrmelendy@gmail.com>
2019-04-05 16:06:30 -0700

    update linked js file; remove if loop which caused an endless loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,48 @@
-//business logic
-// inputNum = [];
-
-var userChoice = [1, 2, 3];
-var numQuote = ["beep", "boop", "I'm sorry, Dave. I'm afraid I can't do that."];
 
 var output = function(userInput) {
-  var userOutput = userInput;
-  // parseInt(userInput);
-  for (var i = userChoice.length()-1; i >=0; i--) {
-    if (userInput === 3) {
-      userOutput = "I'm sorry, Dave. I'm afraid I can't do that.";
-    } else if (userInput === 2) {
-      userOutput = "boop";
-    } else if (userInput === 1) {
-      userOutput = "beep";
-    } else {
-      return userOutput;
-    }
-    console.log(userOutput);
+  var userOutput = "";
+  var userChoice = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var numQuote = ["beep", "boop", "I'm sorry, Dave. I'm afraid I can't do that.", 4, 5, 6, 7, 8, 9];
+  var inputNum = parseInt(userInput);
+  var rangeLength = (inputNum + 1);
 
-}
-  // var rangeOutput = "user";
-  // for (var i = userChoice.length-1; i >= 0; i--) {
-  //   if (userInput >= userChoice[i]) {
-  //     rangeOutput = exceptQuote[i];
-  //   }
-  // }
-  // return rangeOutput;
-  // }
-  //   // if (userInput === 1) {
-  //   //   userInput = "beep";
-  //   //   return userInput;
-  //   // }
-  // console.log(userInput);
+
+  for (var i = rangeLength; 0 <= i <= rangeLength; i --) {
+    userOutput += numQuote[i];
+    console.log(userOutput);
+  };
+
+
+  // If (userInput === 3) {
+  //   userOutput = "I'm sorry, Dave. I'm afraid I can't do that.";
+  // } else if (userInput === 2) {
+  //   userOutput = "boop";
+  // } else if (userInput === 1) {
+  //   userOutput = "beep";
+  // } else {
+  //   return userOutput;
+  // };
+  console.log(userOutput);
+};
+
+
+
+//
+// User Interface Logic
+$(document).ready(function(){
+  $("form#numRange").submit(function(event){
+    event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+    var result = output(userInput);
+    $("#inputRange").text(result);
+    console.log(result);
+  });
 });
 //
-// //UI logic
-// $(document).ready(function(){
-//   $("#numSwitchForm").submit(function(event){
-//     event.preventDefault();
-//     var userInput = parseInt($("#userInput").val());
-//     var result = output(userInput);
-//     $("#romanNumeral").text(result);
-//   });
-// });
+// alert("you entered" + userInput);
+// var output = function(userInput) {
+//   console.log(userInput);
+// };
+//
+//
+//
