1447f651a1c7c6c8d684cfa2b1829f2f384ecf04
Hannah Melendy <hrmelendy@gmail.com>
2019-04-05 14:29:02 -0700

    fix syntax error in html

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ var userChoice = [1, 2, 3];
 var numQuote = ["beep", "boop", "I'm sorry, Dave. I'm afraid I can't do that."];
 
 var output = function(userInput) {
-  var userOutput = "";
+  var userOutput = userInput;
   // parseInt(userInput);
-  for (var i = userInput.length()-1; i >=0; i--) {
+  for (var i = userChoice.length()-1; i >=0; i--) {
     if (userInput === 3) {
       userOutput = "I'm sorry, Dave. I'm afraid I can't do that.";
     } else if (userInput === 2) {
@@ -18,7 +18,6 @@ var output = function(userInput) {
       return userOutput;
     }
     console.log(userOutput);
-  }
 
 }
   // var rangeOutput = "user";
--- js/scriptsTwo.js
+++ js/scriptsTwo.js
@@ -1,29 +1,39 @@
-var arabicNum = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
-var romanNum = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+
+// var arabicNum = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+// var romanNum = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 
 var output = function(userInput) {
   var userOutput = "";
-  for (var i = userInput.length()-1; i >=0; i--) {
-    if (userInput === 3) {
-      userOutput = "I'm sorry, Dave. I'm afraid I can't do that.";
-    } else if (userInput === 2) {
-      userOutput = "boop";
-    } else if (userInput === 1) {
-      userOutput = "beep";
-    } else {
-      return userOutput;
-    }
-    console.log(userOutput);
+//   for (var i = userInput.length()-1; i >=0; i--) {
+  if (userInput === 3) {
+    userOutput = "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if (userInput === 2) {
+    userOutput = "boop";
+  } else if (userInput === 1) {
+    userOutput = "beep";
+  } else {
+    return false;
   }
-  return romanNumOutput;
+  console.log(userOutput);
+  }
+  // return romanNumOutput;
 }
-
+//
 // User Interface Logic
 $(document).ready(function(){
-  $("#numSwitchForm").submit(function(event){
+  $("form#numRange").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#userInput").val());
+    var userInput = parseInt($("input#userInput").val());
     var result = output(userInput);
-    $("#romanNumeral").text(result);
+    $("#result").text(result);
+    console.log(result);
+
+    $("userInput").text(inputFromUser);
   });
 });
+
+alert("you entered" + userInput);
+var output = function(userInput) {
+
+  console.log(userInput);
+}
