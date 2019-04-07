43411b9c76f69893dfc115dae101f95a3b66cbe1
Hannah Melendy <hrmelendy@gmail.com>
2019-04-07 14:25:05 -0700

    fix some bugs in coding

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 var output = function(userInput) {
   var inputNum = parseInt(userInput);
   var rangeLength = (parseInt(inputNum) + 1);
-  var userOutput = "inputNum";
+  var userOutput = inputNum;
 
   console.log(userInput);
-  for (i = rangeLength; 0 <= i <= rangeLength; i--) {
+  for (i = rangeLength; 0 <= i <= rangeLength; i -=i) {
     if (inputNum === 3) {
       userOutput += "I'm sorry, Dave. I'm afraid I can't do that.";
     } else if (inputNum === 2) {
@@ -13,7 +13,7 @@ var output = function(userInput) {
     } else if (inputNum === 1) {
       userOutput = "beep";
     } else {
-      return userOutput;
+      return;
     }
     i -= i;
     console.log(userOutput);
